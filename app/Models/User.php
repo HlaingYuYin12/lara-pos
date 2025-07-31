911b57aaae57400d9e02bd94c279d1f37178e7fe
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-07-31 22:53:54 +0630

    authentication simple login

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'phone','address','image','role'
-    ];
+    protected $fillable = ['name','email','password','phone','address','image','role'];
 
     /**
      * The attributes that should be hidden for serialization.
