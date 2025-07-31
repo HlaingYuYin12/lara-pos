966b7b4da67ffdf1811164dcab5aba52a580f4d5
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-07-31 22:01:04 +0630

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
+            $table->string('address');
+            $table->string('image');
+            $table->char('role',20);
+            // $table->rememberToken();
             $table->timestamps();
         });
 
