210b5bb57f6f9b10b84f0f695d1a5c99f1ee3efa
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-08-07 23:54:21 +0630

    logout

--- resources/views/customer/layouts/master.blade.php
+++ resources/views/customer/layouts/master.blade.php
@@ -74,6 +74,11 @@
                                 </div>
                             </div>
                             <a href="contact.html" class="nav-item nav-link">Contact</a>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <input type="submit" value="Logout" class="btn btn-success">
+                            </form>
                         </div>
                         <div class="d-flex m-3 me-0">
                             <button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
