2613816ed0df3893de3c2b5b8f1de86b8bfcebc3
HlaingYuYin <hlaingyuyin28@gmail.com>
2025-08-13 18:01:24 +0630

    delete unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\AuthController;
-use Laravel\Socialite\Facades\Socialite;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProviderController;
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\User\UserDashboardController;
 use App\Http\Controllers\Admin\AdminDashboardController;
-
-
-// Route::get('/auth/redirect', function () {
-//     return Socialite::driver('github')->redirect();
-// });
-
-// Route::get('/auth/callback', function () {
-//     $user = Socialite::driver('github')->user();
-
-//     // $user->token
-// });
-//or
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\User\UserDashboardController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect']);
 
-Route::get('/auth/{provider}/callback', [ProviderController::class,'callback']);
-
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-//     // dd('hello');
-// });
-
-
+Route::get('/auth/{provider}/callback', [ProviderController::class, 'callback']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () { //login ၀◌င်ပြီးမှအလုပ်လုပ်မယ့်သဘော
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+Route::middleware('auth')->group(function () {
 
     //admin
-    Route::group([ 'prefix' => 'admin' , 'middleware' => 'admin' ],function(){
-        Route::get('/home',[AdminDashboardController::class,'index'])->name('adminDashboard');
+    Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+        Route::get('/home', [AdminDashboardController::class, 'index'])->name('adminDashboard');
 
         //category
         Route::prefix('category')->group(function () {
@@ -55,39 +27,21 @@ Route::middleware('auth')->group(function () { //login ၀◌င်ပြီး�
             Route::get('create', [CategoryController::class, 'createPage'])->name('categoryCreatePage');
             Route::post('create', [CategoryController::class, 'create'])->name('categoryCreate');
 
-
         });
-
 
     });
 
-
-
     //user
-    Route::group([ 'prefix' => 'user' , 'middleware' => 'user'],function(){
-        Route::get('/home',[UserDashboardController::class,'index'])->name('userDashboard');
+    Route::group(['prefix' => 'user', 'middleware' => 'user'], function () {
+        Route::get('/home', [UserDashboardController::class, 'index'])->name('userDashboard');
+
+    });
 
 });
 
-});
-
-require __DIR__.'/auth.php';
-
-
-//admin
-// Route::get('admin/home',function(){
-//     return view('admin.home');
-// })->name('adminDashboard');
-
-
-//customer
-// Route::get('customer/home',function(){
-//     return view('customer.home');
-// })->name('userDashboard');
-
-
+require __DIR__ . '/auth.php';
 
 Route::redirect('/', 'auth/login');
 
-Route::get('auth/register',[AuthController::class,'registerPage'])->name('userRegister');
-Route::get('auth/login',[AuthController::class,'loginPage'])->name('userLogin');
+Route::get('auth/register', [AuthController::class, 'registerPage'])->name('userRegister');
+Route::get('auth/login', [AuthController::class, 'loginPage'])->name('userLogin');
